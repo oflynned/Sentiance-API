c698134f3aa24aae1d0cba61b3abc3a018002b7c
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-07 14:56:15 +0200

    add example tests to force workflow on github workflow

---
+++ src/tests/example.integration.ts
@@ -0,0 +1,5 @@
+describe('on integration test workflow forced', () => {
+  it('should run', () => {
+    expect(2 + 2).toEqual(4);
+  });
+});
---
+++ src/tests/example.spec.ts
@@ -0,0 +1,5 @@
+describe('on unit test workflow forced', () => {
+  it('should run', () => {
+    expect(2 + 2).toEqual(4);
+  });
+});
