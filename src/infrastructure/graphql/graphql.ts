6f18c8f613cda8c49940070c0305196ed513480a
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-07 21:08:31 +0200

    hide stacktraces on production

--- src/infrastructure/graphql/graphql.ts
+++ src/infrastructure/graphql/graphql.ts
@@ -3,20 +3,19 @@ import { Application } from 'express';
 import { Server as HttpServer, createServer } from 'http';
 import { typeDefs } from './definitions';
 import { resolvers } from './resolvers';
+import { Environment } from '../../config/environment';
 
 export const graphql = (app: Application): HttpServer => {
   const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
     playground: true,
-    introspection: true
+    introspection: true,
+    debug: !Environment.isProduction()
     // playground: Environment.isDevelopment(),
     // introspection: Environment.isDevelopment()
   });
+
   apolloServer.applyMiddleware({ app });
-
-  const httpServer = createServer(app);
-  apolloServer.installSubscriptionHandlers(httpServer);
-
-  return httpServer;
+  return createServer(app);
 };
